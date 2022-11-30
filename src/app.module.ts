9152e6545eea2cdb19dad27c33c7b52b64cec44f
Leonel Villagra <leonel.villagrar@gmail.com>
2022-11-30 01:41:27 -0300

    add finals CRUD and migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,44 @@
 import { Module } from '@nestjs/common';
 
 import {
+  AreasCategoryModule,
+  AreasModule,
+  BranchOfficesModule,
+  ClientsModule,
   CommunesModule,
+  DeliveryZonesModule,
+  DistrictsModule,
+  MonthlyAreaPopulationProjectionsModule,
+  MonthlyCommunalPopulationProjectionsModule,
+  ProjectedConsumptionsModule,
+  ProteinSectorsModule,
   ProvincesModule,
+  RealConsumptionsModule,
   RegionsModule,
   SalesChannelModule,
+  SalesModule,
 } from './modules';
+
 import { DatabaseModule } from './infraestructure';
 @Module({
   imports: [
     DatabaseModule,
-    CommunesModule,
-    ProvincesModule,
     RegionsModule,
+    ProvincesModule,
+    CommunesModule,
+    BranchOfficesModule,
+    DistrictsModule,
+    AreasModule,
+    MonthlyAreaPopulationProjectionsModule,
+    DeliveryZonesModule,
+    ClientsModule,
+    MonthlyCommunalPopulationProjectionsModule,
     SalesChannelModule,
+    SalesModule,
+    ProteinSectorsModule,
+    ProjectedConsumptionsModule,
+    RealConsumptionsModule,
+    AreasCategoryModule,
   ],
   controllers: [],
   providers: [],
